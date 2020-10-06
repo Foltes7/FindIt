bcf4f634aa4dc1e6bec0ebf76262d89f825ad5ca
Davyd Konopatskyi <40700475+Foltes7@users.noreply.github.com>
2020-10-06 21:52:08 +0300

    new arch

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,30 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContentComponent } from './content/content/content.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
-  },
-  {
-    path: 'saved',
-    loadChildren: () => import('./saved/saved.module').then(m => m.SavedModule),
-  },
-  {
-    path: 'search',
-    loadChildren: () => import('./saved/saved.module').then(m => m.SavedModule),
-  },
-  {
-    path: 'messages',
-    loadChildren: () => import('./messages/messages.module').then(m => m.MessagesModule),
+    component: ContentComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+      },
+      {
+        path: 'saved',
+        loadChildren: () => import('./saved/saved.module').then(m => m.SavedModule),
+      },
+      {
+        path: 'search',
+        loadChildren: () => import('./saved/saved.module').then(m => m.SavedModule),
+      },
+      {
+        path: 'messages',
+        loadChildren: () => import('./messages/messages.module').then(m => m.MessagesModule),
+      },
+      {
+        path: 'profile/:id',
+        loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule),
+      },
+    ]
   },
   {
     path: 'about',
     loadChildren: () => import('./about/about.module').then(m => m.AboutModule),
-  },
-  {
-    path: ':id',
-    loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule),
   },
   {
     path: '**',
---
+++ Client/src/app/content/content.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContentComponent } from './content/content.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [ContentComponent],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class ContentModule { }
