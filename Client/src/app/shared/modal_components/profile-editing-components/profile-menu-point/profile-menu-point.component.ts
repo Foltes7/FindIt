7bd68040d8a9b0703bdc477abee3ed3710c2f79c
Davyd Konopatskyi <davydqq@gmail.com>
2021-01-08 23:03:14 +0200

    add states groups

--- Client/src/app/shared/modal_components/profile-editing-components/profile-menu-point/profile-menu-point.component.ts
+++ Client/src/app/shared/modal_components/profile-editing-components/profile-menu-point/profile-menu-point.component.ts
@@ -1,4 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+
+export interface StateGroup {
+  letter: string;
+  names: string[];
+}
+
+export const _filter = (opt: string[], value: string): string[] => {
+  const filterValue = value.toLowerCase();
+
+  return opt.filter(item => item.toLowerCase().indexOf(filterValue) === 0);
+};
 
 @Component({
   selector: 'app-profile-menu-point',
@@ -7,10 +21,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileMenuPointComponent implements OnInit {
 
+  public mainForm: FormGroup = new FormGroup({
+    userName: new FormControl('',  [Validators.required, Validators.minLength(4), Validators.maxLength(45)]),
+    name: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(45) ]),
+    stateGroup: new FormControl('')
+  });
+  
+  stateGroupOptions: Observable<StateGroup[]>;
   user:any;
+  stateGroups: StateGroup[] = [{
+    letter: 'A',
+    names: ['Alabama', 'Alaska', 'Arizona', 'Arkansas']
+  }, {
+    letter: 'C',
+    names: ['California', 'Colorado', 'Connecticut']
+  }, {
+    letter: 'D',
+    names: ['Delaware']
+  }, {
+    letter: 'F',
+    names: ['Florida']
+  }, {
+    letter: 'G',
+    names: ['Georgia']
+  }, {
+    letter: 'H',
+    names: ['Hawaii']
+  }, {
+    letter: 'I',
+    names: ['Idaho', 'Illinois', 'Indiana', 'Iowa']
+  }, {
+    letter: 'K',
+    names: ['Kansas', 'Kentucky']
+  }, {
+    letter: 'L',
+    names: ['Louisiana']
+  }, {
+    letter: 'M',
+    names: ['Maine', 'Maryland', 'Massachusetts', 'Michigan',
+      'Minnesota', 'Mississippi', 'Missouri', 'Montana']
+  }, {
+    letter: 'N',
+    names: ['Nebraska', 'Nevada', 'New Hampshire', 'New Jersey',
+      'New Mexico', 'New York', 'North Carolina', 'North Dakota']
+  }, {
+    letter: 'O',
+    names: ['Ohio', 'Oklahoma', 'Oregon']
+  }, {
+    letter: 'P',
+    names: ['Pennsylvania']
+  }, {
+    letter: 'R',
+    names: ['Rhode Island']
+  }, {
+    letter: 'S',
+    names: ['South Carolina', 'South Dakota']
+  }, {
+    letter: 'T',
+    names: ['Tennessee', 'Texas']
+  }, {
+    letter: 'U',
+    names: ['Utah']
+  }, {
+    letter: 'V',
+    names: ['Vermont', 'Virginia']
+  }, {
+    letter: 'W',
+    names: ['Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
+  }];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.stateGroupOptions = this.mainForm.get('stateGroup')!.valueChanges
+    .pipe(
+      startWith(''),
+      map(value => this._filterGroup(value))
+    );
+  }
+
+  private _filterGroup(value: string): StateGroup[] {
+    if (value) {
+      return this.stateGroups
+        .map(group => ({letter: group.letter, names: _filter(group.names, value)}))
+        .filter(group => group.names.length > 0);
+    }
+
+    return this.stateGroups;
   }
 
 }
